4e7b446d2f6c641c20a98b47725dc1e9325bab68
evgen_kolesnik <deimos0233@gmail.com>
2021-06-25 15:30:34 +0300

    solution: Task01

---
+++ app/src/main/java/com/example/test_app/FirstFragment.kt
@@ -0,0 +1,63 @@
+package com.example.test_app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class FirstFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    private fun initRecyclerView(view: View) {
+        val recyclerView: RecyclerView = view.findViewById(R.id.rView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = CustomAdapter(getCountries())
+    }
+
+    private fun getCountries(): List<String> {
+        return this.resources.getStringArray(R.array.countries).toList()
+    }
+
+/*    fun click(view: View) {
+        val button = view.findViewById<Button>(R.id.mainButton)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        initRecyclerView(view)
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FirstFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
